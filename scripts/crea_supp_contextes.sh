67d66c8956a5a0b94f1661500a483f3c0d5808af
JuliePradelli <juliepradelli@gmail.com>
2017-01-23 09:12:57 +0100

    Update crea_supp_contextes.sh

--- scripts/crea_supp_contextes.sh
+++ scripts/crea_supp_contextes.sh
@@ -34,9 +34,57 @@ then
 		# ******************** DESTRUCTION CONTEXTE ********************************
 		elif [[ "$1" -eq 1 && "$existence" -eq 1 ]]
 		then
-			# verifier si le contexte est plein
+			# verifier si le contexte est plein et si oui réecrire les lignes dans le default
 			
+			i=0
+			tab=()
+			tab1=()
+			tab2=()
+			tab3=()
+			while read ligne
+			do
+				tab[$i]="$ligne"
+				i=$(($i+1))
+			done < /usr/local/var/lib/asterisk/context/esiea.conf
 
+			m=0
+			n=0
+			for ((j=1; j<i; j++))
+			do
+				if [[ ${tab[$j]} =~ ^[exten].+$ ]]
+				then
+					tab3[$n]=${tab[$j]}
+					n=$(($n+1))
+					num=`echo ${tab[$j]} | cut -d' ' -f3`
+					exten=`echo $num | cut -d',' -f1`
+					find /usr/local/var/lib/asterisk/users/*.conf > a.txt
+					i=0
+					while read ligne
+					do
+						tab1[$i]="$ligne"
+						i=$(($i+1))
+					done < /var/www/script/a.txt
+					for ((l=0; l<i; l++))
+					do
+						while read ligne1
+						do
+							if [[ $ligne1 == "[$exten]" ]]
+							then
+								tab2[$m]=${tab1[$l]}
+								m=$(($m+1))
+							fi
+						done < ${tab1[$l]}
+					done
+				fi
+			done
+
+			for ((j=0; j<m; j++))
+			do
+				sed -I -e -E 's/context=.+/context=default/g' ${tab2[$j]}
+			done
+
+			# ******************** POURSUIVRE LA DESCTRUCTION DU CONTEXTE ********************
+			
 			rm /usr/local/var/lib/asterisk/context/$2.conf
 			chemin1="#include \"/usr/local/var/lib/asterisk/context/$2.conf\""
 			ligne1=`grep -n "$chemin1" /usr/local/etc/asterisk/extensions.conf`
@@ -58,7 +106,7 @@ then
 			echo "Si vous voulez ajouter, le fichier existe deja; si vous voulez supprimer, le fichier n'existe pas."
 		fi
 	else
-		echo "Le premier champ est mal renseign�."
+		echo "Le premier champ est mal renseignÃ."
 	fi
 else
 	echo "Veuillez renseigner les 3 parametres (creation(0)/suppression(1) - nom - contexte utilisateur(0)/gtw(1))."
